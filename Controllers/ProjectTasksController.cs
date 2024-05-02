177407850c97b4eb6ec0ac9c900a0ee186af2a08
Mihael Ladić <mladic2001@gmail.com>
2024-05-02 16:23:04 +0200

    * Project tasks controller

---
+++ Controllers/ProjectTasksController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialEduApi.Data;
+using SocialEduApi.Models.Entities;
+using SocialEduApi.Models.ViewModels;
+
+namespace SocialEduApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectTasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectTasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProjectTasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProjectTask>> GetProjectTask(int id)
+        {
+            var projectTask = await _context.ProjectTasks.FindAsync(id);
+
+            if (projectTask == null)
+            {
+                return NotFound();
+            }
+
+            return projectTask;
+        }
+
+        // POST: api/ProjectTasks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProjectTask>> PostProjectTask(ProjectTaskVM vm)
+        {
+            if (vm.SubjectID == null) return BadRequest("Fali subject ID");
+            if (vm.Title == null) return BadRequest("Fali naslov");
+            if (vm.Description == null) return BadRequest("Fali opis");
+            if (vm.Criteria == null) return BadRequest("Fali kriterij");
+            if (vm.MaxGrade == null) return BadRequest("Fali maksimalna ocjena");
+
+            var projectTask = vm.GetProjectTask();
+            projectTask.Id = 0;
+            projectTask.Subject = null;
+            projectTask.CreatedDate = DateTime.Now;
+
+            _context.ProjectTasks.Add(projectTask);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProjectTask", new { id = projectTask.Id }, projectTask);
+        }
+
+        // DELETE: api/ProjectTasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProjectTask(int id)
+        {
+            var projectTask = await _context.ProjectTasks.FindAsync(id);
+            if (projectTask == null)
+            {
+                return NotFound();
+            }
+
+            var submissions = _context.ProjectSubmissions.Where(s => s.ProjectTaskID == id);
+            _context.ProjectSubmissions.RemoveRange(submissions);
+            _context.ProjectTasks.Remove(projectTask);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
